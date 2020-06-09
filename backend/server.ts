9cda9464dc6be0301ad8c4fce35711b86c2a2ff5
rushi444 <rushi44@gmail.com>
2020-06-08 19:26:03 -0700

    binary error, not working b/c windows

--- backend/server.ts
+++ backend/server.ts
@@ -4,9 +4,12 @@ import { schema } from './src/schema'
 
 const app = new ApolloServer({ schema, context: createContext });
 
-exports.graphqlHandler = app.createHandler({
-  cors: {
-    origin: '*',
-    credentials: true
-  }
-});
+exports.graphqlHandler = (event: any, context: any, callback: any) => {
+  const handler = app.createHandler({
+    cors: {
+      origin: '*',
+      credentials: true
+    }
+  })
+  return handler(event, context, callback)
+};
